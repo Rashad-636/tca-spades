db48a9620b24399090f570396892ade7d9f39653
Rashad Adams <Rashaddms@gmail.com>
2023-04-18 14:38:20 -0400

    Code along to set up storage

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Homepage, Setup, GameInPlay } from "./pages";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import localforage from 'localforage';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const hardcodedGameResults= [
   {
@@ -19,6 +21,8 @@ const hardcodedGameResults= [
 
 const App = () => {
 
+  // State Hooks
+
   const [gameresults, setGameResults] = useState(hardcodedGameResults);
 
   const [setupInfo, setSetupInfo] = useState({
@@ -29,10 +33,62 @@ const App = () => {
     playerFour: ""
   });
 
+  const [emailKey, setEmailKey] = useState("");
+
+  // useEffect hook
+
+  useEffect(
+    () => {
+
+        const loadEmailKey = async () => {
+
+            try {
+                  setEmailKey(
+                    await localforage.getItem("emailKey") ?? ""
+                  );
+            }
+            catch (err) {
+              console.error(err)
+            }
+        };
+
+        loadEmailKey();
+    }
+    , []
+  );
+
+  // helper functions
+
+	const saveEmailKey = async () => {
+		try {
+			await localforage.setItem(
+				"emailKey"
+				, emailKey
+			);
+		}
+		catch (err) {
+			console.error(err);
+		}
+	};
+
   return (
     <div className="App">
 
       <h1>Spades Companion App</h1>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+				<Form.Control 
+					type="text" 
+					placeholder="Enter Email"
+					value={emailKey} 
+					onChange={(e) => setEmailKey(e.target.value)}
+				/>
+        <br />
+				<Button
+					onClick={saveEmailKey}
+				>
+					Save
+				</Button>
+			</Form.Group>
       <hr />
       <HashRouter>
         <Routes>
